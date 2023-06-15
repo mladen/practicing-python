6839e227147c6b561c4d9843fce5d66bfda957a9
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-15 22:33:45 +0200

    Adds 'lists' and 'tuples' examples

---
+++ 8-lists-and-tuples.py
@@ -0,0 +1,14 @@
+# Lista
+fruits = ['apple', 'pear', 3] # Ovo je kao "niz" u JavaScript-i i PHP-u
+print(fruits[2])
+
+fruits.append('strawberry')
+print(fruits)
+
+# Tuple
+position = (2, 3) # koordinate
+print(position)
+
+color = (255, 255, 4) # boja
+print(color)
+
