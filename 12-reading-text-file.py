5228e1472e3e1b6a98e5216cfd51d5b30fab9062
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-16 18:05:22 +0200

    Updates the 'reading text file' a bit

--- 12-reading-text-file.py
+++ 12-reading-text-file.py
@@ -1,3 +1,13 @@
 file = open('testfile.txt', 'r')
 f = file.readlines()
 print(f) # ['Test text in a file called testfile.txt!\n', '\n', 'Third line...\n', '\n', '\n', '6th line...']
+
+# To remove the '\n' symbols from the result
+newList = []
+for line in f:
+    if line[-1] == '\n': # dakle, propusti samo one stringove koji na kraju imaju simbol '\n'
+        newList.append(line[:-1]) # koristimo slice operator da bismo uklonili '\n'
+    else:
+        newList.append(line) # dodajemo poslenji element, koji smo izuzeli sa 'if' izrazom jer nije imao simbol '\n'
+
+print(newList)
