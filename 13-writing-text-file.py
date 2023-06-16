c907af30b0f06cdf5c79160bebfbf86d7a1bd08d
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-16 21:21:55 +0200

    Changes the way I entered the previous text on a new line

--- 13-writing-text-file.py
+++ 13-writing-text-file.py
@@ -1,4 +1,4 @@
 file = open('testfile-writing.txt', 'w')
-file.write('Entering new file\n')
-file.write('Entering a new line')
+file.write('Entering new file')
+file.write('\nEntering a new line')
 file.close()
