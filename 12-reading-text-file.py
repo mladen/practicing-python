5c70a4a4e6d8ed9a080502456702f85e740524a1
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-16 21:17:21 +0200

    Changes the name of the (only test text file so far) file to 'testfile-reading.txt'

--- 12-reading-text-file.py
+++ 12-reading-text-file.py
@@ -1,4 +1,4 @@
-file = open('testfile.txt', 'r')
+file = open('testfile-reading.txt', 'r')
 f = file.readlines()
 print(f) # ['Test text in a file called testfile.txt!\n', '\n', 'Third line...\n', '\n', '\n', '6th line...']
 
