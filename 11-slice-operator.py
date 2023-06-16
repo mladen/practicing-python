5c3a282ec8f3301fbeaecac5a68ded10d352b6e1
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-16 11:05:50 +0200

    Adds the 'slice operator' example, for list and string

---
+++ 11-slice-operator.py
@@ -0,0 +1,9 @@
+fruits = ['apple', 'pear', 'strawberry']
+
+# print(text[start:stop:step])
+print(fruits[0:2]) # daje ['apple', 'pear'], dakle UZIMA element sa indeksom 0 i ide DO elementa sa indeksom 2 (ali ga izuzima)
+
+text = 'Hi there, I like Python'
+print(text[0:4]) # daje 'Hi t'
+
+
