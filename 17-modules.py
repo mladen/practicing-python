74fd5c7f0a2fc11aacf96b3f40cf98e6b145b2f4
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-19 19:56:00 +0200

    Adds the 'modules' example (which includes the test module)

---
+++ 17-modules.py
@@ -0,0 +1,5 @@
+import testmodule
+
+testmodule.printName()
+# testmodule.printName() # Kada ne proslijedimo string/ime onda se koristi
+                         # 'default'-na vrijednost iz fajla testmodule.py
---
+++ testmodule.py
@@ -0,0 +1,2 @@
+def printName(name = 'Marko'): # Ako ne proslijedimo string (u glavnom fajlu) onda se uzima ova vrijednost
+    print(name)
