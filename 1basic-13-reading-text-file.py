e6e9b2c4eeb4dd824fdece6aa240804a2cc8240e
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-20 19:15:05 +0200

    Renames the files to mark them as the ones that belong to the Basic/Beginner Python course

--- 1basic-13-reading-text-file.py
+++ 1basic-13-reading-text-file.py
@@ -1,4 +1,4 @@
-file = open('testfile-reading.txt', 'r')
+file = open('1basic-testfile-reading.txt', 'r')
 f = file.readlines()
 print(f) # ['Test text in a file called testfile.txt!\n', '\n', 'Third line...\n', '\n', '\n', '6th line...']
 
--- 1basic-14-writing-text-file.py
+++ 1basic-14-writing-text-file.py
@@ -1,4 +1,4 @@
-file = open('testfile-writing.txt', 'w')
+file = open('1basic-testfile-writing.txt', 'w')
 file.write('Entering new file')
 file.write('\nEntering a new line')
 file.close()
