548147370ded247faa71c84cf5bdc96228298684
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-23 20:16:23 +0200

    Updates the 'creating classes' example with retreiving the param's values

--- 2oop-creating-classes.py
+++ 2oop-creating-classes.py
@@ -12,8 +12,19 @@ class User(object):
         print('Hi! I am ', self.name, 'and I am', self.age, 'years old')
         # pass
 
+    def change_age(self, age):
+        self.age = age
+
 tim = User('Tim', 46) # Tim je instanca klase User
 fred = User('Fred', 52) # Fred je instanca klase User
 
 tim.speak()
 fred.speak()
+
+tim.change_age(47)
+tim.speak()
+
+# Ako zelimo samo da dobijemo koliko godina ima npr. Tim (sto se cuva u atributu)
+# a da, pri tome, ne istampamo cijelu poruku iz funkcije speak()
+# onda samo radimo:
+print('\n', tim.age)
