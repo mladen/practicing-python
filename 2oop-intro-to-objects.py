b7a66e0267d71c2137a0dd186f5a46e6a67588f6
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-23 15:31:31 +0200

    Moves the 'Turtle' example to another file; adds the main 'objects' example

--- 2oop-intro-to-objects.py
+++ 2oop-intro-to-objects.py
@@ -1,12 +1,5 @@
-import turtle
+x = 5
+y = 'string'
 
-# Create a turtle object
-my_turtle = turtle.Turtle()
-
-# Move the turtle forward and turn right to create a square
-for _ in range(4):
-    my_turtle.forward(100)
-    my_turtle.right(90)
-
-# Close the turtle graphics window
-turtle.done()
+print(type(x)) # Daje <class 'int'>
+print(type(y)) # Daje <class 'str'>
