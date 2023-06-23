3d75bff3022edcf75164dd048d4eb53e399bbc18
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-23 16:49:40 +0200

    Updates the 'intro to objects' example

--- 2oop-intro-to-objects.py
+++ 2oop-intro-to-objects.py
@@ -3,3 +3,14 @@ y = 'string'
 
 print(type(x)) # Daje <class 'int'>
 print(type(y)) # Daje <class 'str'>
+
+print(y.upper()) # Metoda je funkcija koju pozivamo na(d) objektom
+                 # Metode nam takodje vracaju nesto
+                 # kao npr. string gdje su sva slova velika
+                 # TODO: Da li sve metode vracaju nesto?
+
+                 # Nekada i metode mogu uzeti objekte.
+
+def testfunc(x):
+    return x + 1
+print(testfunc(5)) # Funkcija uzima objekat/objekte i primjenjuje neku operaciju nad njima
