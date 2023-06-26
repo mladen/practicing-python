e5e2dc8e1de73b88354d94be78ea06716e49e48f
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-26 20:24:38 +0200

    Updates the 'overloading methods' with gt, ge, lt, le, eq methods

--- 2oop-4-overloading-methods.py
+++ 2oop-4-overloading-methods.py
@@ -15,8 +15,29 @@ class Point():
         return Point(self.x - p.x, self.y - p.y)
 
     def __mul__(self, p):
-        return Point(self.x * p.x, self.y * p.y)
-    
+        return self.x * p.x + self.y * p.y # U matematici, mnozenje vraca skalar a ne vektor
+
+    # Greater than, Greater or equal, Less then, Less or equal, Equal
+    def length(self): # Prvo nam treba funkcija za racunanje duzine vektora
+        import math
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __gt__(self, p):
+        return self.length() > p.length()
+
+    def __ge__(self, p):
+        return self.length() >= p.length()
+
+    def __lt__(self, p):
+        return self.length() < p.length()
+
+    def __le__(self, p):
+        return self.length() <= p.length()
+
+    def __eq__(self, p):
+        # return self.length() == p.length()
+        return self.x == p.x and self.y == p.y # Drugi nacin, bez koriscenja length()
+
     def __str__(self) -> str: # Ova metoda se poziva svaki put kada Point
                               # objekat trebamo da konvertujemo u string
                               # Npr. kada treba da istampamo objekat sa print()
@@ -29,7 +50,11 @@ p1 = Point(3, 4)
 p2 = Point(3, 2)
 p3 = Point(1, 3)
 p4 = Point(0, 1)
+p5 = Point(3, 4)
 
 print(p1 + p2)
 print(p1 - p2)
 print(p1 * p2)
+
+print(p1 > p2)
+print(p1 == p5)
