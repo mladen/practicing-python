121fe0b7f25e2f8217aca676b9afc995dafa4300
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-27 18:53:41 +0200

    Adds the 'public and private classes' example

---
+++ 2oop-public-and-private-classes.py
@@ -0,0 +1,16 @@
+class _Private: # Mogu da se koriste unutar fajla/opsega (scope-a)
+    name = ''
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+class NotPrivate: # Moze da im svako pristupi
+    def __init__(self, name) -> None:
+        self.name = name
+        self.priv = _Private(name)
+
+    def _display(self):
+        print('Hello')
+
+    def display(self):
+        print('Hi')
