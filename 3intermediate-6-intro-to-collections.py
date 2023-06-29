0c53c46ef0726d326c3f83031c56b6cebcb06b50
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-06-30 01:35:10 +0200

    Updates the 'intro to collections' with Counter() manipulations

--- 3intermediate-6-intro-to-collections.py
+++ 3intermediate-6-intro-to-collections.py
@@ -33,18 +33,58 @@ c3 = Counter({"a": 1, "b": 3, "a": 2, "a": 16, "a": 2})  # Stavljamo dictionary
 print(c3)
 
 # 4. Keywords
-# c4 = Counter(
-#     cats=4, dogs=2
-# )  # Stavljamo keywords (TODO: pogledati i ovo - sta je u pitanju)
+c4 = Counter(
+    cats=4, dogs=2
+)  # Stavljamo keywords (TODO: pogledati i ovo - sta je u pitanju)
 # Ustvari ovo prethodno je rezultat sledeceg koda? To je poenta valjda?
-c4 = Counter(["cat", "cat", "dog", "cat", "dog", "cat"])
+# c4 = Counter(["cat", "cat", "dog", "cat", "dog", "cat"])
 print(c4)
 
-# 5. Pristupanje elementima
+# ##########################################################################
+
+# 4.1 Pristupanje elementima
 print(c4["cat"])
 print(c4["elephant"])  # Kod liste, kada trazimo nepostojeci clan, ovo daje nulu (0)
 
-testDict = {"dog": 2, "cat": 1}
+# testDict = {"dog": 2, "cat": 1}
+# print(
+#     testDict["rhino"]
+# )  # Kod dictionary ako trazimo nepostojeci kljuc - dobijamo gresku
+
+# 4.2 Izlistavanje svih elemenata iz primjera pod 4.
+print(list(c4.elements()))  # Sa ovim list() mi KONVERTUJEMO nesto u listu
+
+# 4.3 Izlistavanje najcescih elemenata
+print(c4.most_common(2))  # Izlistavamo dva najcesce koriscena elementa iz c4
+
+# 4.3 Subtracting
+sub = [
+    "cats",
+    "cats",
+]  # Napomena: Ovdje mozemo da koristimo: dictionary, Counter object, set, tuple...
+c4.subtract(sub)  # Dakle, od c4 se oduzimaju elementi iz sub liste
+print("Subtracting: ", c4)
+
+# 4.4 Update
+upd = [
+    "dogs"
+]  # Napomena: Ovdje mozemo da koristimo: dictionary, Counter object, set, tuple...
+c4.update(upd)
+print("Updating: ", c4)
+
+# 4.5 Clear (removes all of the counts)
+c4.clear()
+print("Clear: ", c4)
+
+# 4.6 Adding and Subtracting
+z = Counter(a=4, b=2, c=0, d=-2)
+r = Counter(["a", "b", "b", "c"])
+print("Addition: ", z + r)
 print(
-    testDict["rhino"]
-)  # Kod dictionary ako trazimo nepostojeci kljuc - dobijamo gresku
+    "Subtraction:", z - r
+)  # U rezultatu ovoga nam se nece prikazati b, c, i d jer ako je
+# Count elementa manji ili jednak nuli, element nece bit prikazan u Counter-u
+
+# 4.7 Intersection and Union
+print("Intersection: ", z & r)  # Uzima minimalne elemente???
+print("Union: ", z | r)  # Uzima maksimalne elemente
