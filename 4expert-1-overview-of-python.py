5b65945e997342636635405c86c6448494c0ca9c
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-07-04 11:00:19 +0200

    Adds the 'overview of python' (expert level)

---
+++ 4expert-1-overview-of-python.py
@@ -0,0 +1,9 @@
+# Python je interpreted programming language
+# Python se prvo kompajlira u bytecode prije nego sto se interpretira
+
+# Sta je kompajler a sta je interpreter?
+# KOMPAJLER uzima high-level code i prevodi ga u lower level code
+# INTERPRETER uzima neki kod (tipicno bytecode) i interpretira i pokrece (runs)
+# taj kod tj. cita (reads) taj kod i prevodiga na u trenutku (on the fly)
+# u masinski kod koji moze da bude izvrsen od strane naseg racunara (za razliku
+# od kompajlera koji to radi prije)
