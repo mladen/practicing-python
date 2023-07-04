633021c2ecb9bb009441702071cf81be101da9cb
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-07-04 19:24:50 +0200

    Updates the 'overview of python' with make_class()

--- 4expert-1-overview-of-python.py
+++ 4expert-1-overview-of-python.py
@@ -7,3 +7,20 @@
 # taj kod tj. cita (reads) taj kod i prevodiga na u trenutku (on the fly)
 # u masinski kod koji moze da bude izvrsen od strane naseg racunara (za razliku
 # od kompajlera koji to radi prije)
+
+
+def make_class(x):
+    class Dog:
+        def __init__(self, name):
+            self.name = name
+
+        def print_value(self):
+            print(x)
+
+    return Dog
+
+
+cls = make_class(10)
+d = cls("Mladen")
+print(d.name)
+d.print_value()
