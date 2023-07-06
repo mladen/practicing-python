e8a7a04c669e7790c3875c1118dbf7c562ca7bf7
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-07-06 22:12:06 +0200

    Updates the 'generators' with an example

--- 4expert-generators.py
+++ 4expert-generators.py
@@ -14,3 +14,29 @@
 # jednu vrijednost u odredjenom momentu a da ne smjestamo
 # cijelu sekvencu (npr. brojeva) u memoriju, ako nam to
 # vec nije potrebno.
+
+
+class Gen:
+    def __init__(self, n):
+        self.n = n
+        self.last = 0
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.last == self.n:
+            raise StopIteration()
+
+        rv = self.last**2
+        self.last += 1
+        return rv
+
+
+g = Gen(100)
+
+while True:
+    try:
+        print(next(g))
+    except StopIteration:
+        break
