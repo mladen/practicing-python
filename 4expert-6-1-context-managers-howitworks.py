ee88bb8044ea919419e29ae096c9d26269c59b55
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2023-07-07 22:12:46 +0200

    Updates the 'context managers how it works' by adding/handling Exceptions

--- 4expert-6-1-context-managers-howitworks.py
+++ 4expert-6-1-context-managers-howitworks.py
@@ -9,13 +9,17 @@ class File:
     # __exit__ se poziva nevezano da li je bilo exception-a
     # izmedju enter i exit, i ovdje se deal-uje sa exception-om
     def __exit__(self, type, value, traceback):
+        print(f"{type}, {value}, {traceback}")
         print("Exit")
         self.file.close()
+        if type == Exception:
+            return True
 
 
 with File("1basic-testfile-writing.txt", "w") as f:
     print("Middle")
     f.write("helloooo")
-    raise Exception()
-    # Iako smo exception/gresku, bice pozvana __exit__()
-    # sto ce pravilno zatvoriti fajl
+    raise FileExistsError()
+    # raise Exception()
+    # Iako smo stavili exception/gresku na Middle koraku,
+    # bice pozvana __exit__() sto ce pravilno zatvoriti fajl
