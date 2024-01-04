29dcd3498a450fa1cbe2c91d4bd3b1e18b99a767
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-01-04 11:46:40 +0100

    Refactors conditional operators in comparing_numbers.py

--- tech_with_tim-learn_python_in_75_minutes/11conditional_operators-comparing_numbers.py
+++ tech_with_tim-learn_python_in_75_minutes/11conditional_operators-comparing_numbers.py
@@ -1,18 +1,18 @@
-result = (
+result1 = (
     6 == 6
-)  # The double equal sign is used to compare two values. Then it returns a boolean value to the variable "result".
-print(result, end="\n\n")
+)  # The double equal sign is used to compare two values. Then it returns a boolean value to the variable "result"
+print("result1:", result1, end="\n\n")
 
-result = 6.9 >= 7
-print(result, end="\n\n")
+result2 = 6.9 >= 7
+print("result2:", result2, end="\n\n")
 
-result = 7 <= 7.1
-print(result, end="\n\n")
+result3 = 7 <= 7.1
+print("result3:", result3, end="\n\n")
 
 x = 5
-result = 4 < x < 8  # You can compare multiple values at once.
-# result = 4 < x and x < 8  # This is the same as the previous line.
-print(result, end="\n\n")
+result4 = 4 < x < 8  # You can compare multiple values at once
+# result4 = 4 < x and x < 8  # This is the same as the previous line
+print("result4:", result4, end="\n\n")
 
-result = 4 < x + 1  # The "less than" is evaluated last.
-print(result, end="\n\n")
+result5 = 4 < x + 1  # The "less than" is evaluated last
+print("result5:", result5, end="\n\n")
