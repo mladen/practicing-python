5e06603fc9c3db7633f4d67c47894db8feef0a86
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-01-04 10:54:34 +0100

    Adds more conditional operator examples

--- tech_with_tim-learn_python_in_75_minutes/11conditional_operators-comparing_numbers.py
+++ tech_with_tim-learn_python_in_75_minutes/11conditional_operators-comparing_numbers.py
@@ -8,3 +8,11 @@ print(result, end="\n\n")
 
 result = 7 <= 7.1
 print(result, end="\n\n")
+
+x = 5
+result = 4 < x < 8  # You can compare multiple values at once.
+# result = 4 < x and x < 8  # This is the same as the previous line.
+print(result, end="\n\n")
+
+result = 4 < x + 1  # The "less than" is evaluated last.
+print(result, end="\n\n")
