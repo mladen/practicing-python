fc18bdbbf1bf6420424358d726aa467f6296a4b2
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-01-04 10:38:48 +0100

    Adds conditional operator examples and updates one filename's title

---
+++ tech_with_tim-learn_python_in_75_minutes/11conditional_operators-comparing_numbers.py
@@ -0,0 +1,10 @@
+result = (
+    6 == 6
+)  # The double equal sign is used to compare two values. Then it returns a boolean value to the variable "result".
+print(result, end="\n\n")
+
+result = 6.9 >= 7
+print(result, end="\n\n")
+
+result = 7 <= 7.1
+print(result, end="\n\n")
