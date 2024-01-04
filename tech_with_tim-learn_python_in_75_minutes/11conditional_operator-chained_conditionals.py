5951fb39345207b9e6e37f270f5616ff32436da4
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-01-04 12:04:23 +0100

    Updates the chained conditionals example

--- tech_with_tim-learn_python_in_75_minutes/11conditional_operator-chained_conditionals.py
+++ tech_with_tim-learn_python_in_75_minutes/11conditional_operator-chained_conditionals.py
@@ -1,5 +1,6 @@
-# We can use "and" and "or" to combine multiple conditions
+# We can use "and", "or" and "not" to combine multiple conditions
 
+# Examples with "and"
 result1 = 3 != 3
 result2 = 3 < 5
 result3 = (
@@ -13,3 +14,10 @@ result4 = (
     result1 or not result2 or result3  # "not" is used to reverse the boolean value
 )  # "or" is used to compare two boolean values; if one of them is true, the result is true
 print("result4:", result4, end="\n\n")
+
+# Example with "not"
+print(
+    not (False or True)  # We can use parentheses to change the order of evaluation
+)  # The result is false because the parentheses are evaluated first ("False or True" gives True)
+
+# IMPORTANT: Order of operations is "not", "and", "or"
