67ce688e61d1448a5ac7b3f0d13b18ade9f59fb9
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-06-18 14:04:20 +0200

    Improves the 21exerciseSuperList.py by simply inheriting from the "list"

--- zero_to_mastery-complete_python_developer/6advanced_python_oop/21exerciseSuperList.py
+++ zero_to_mastery-complete_python_developer/6advanced_python_oop/21exerciseSuperList.py
@@ -1,23 +1,26 @@
-class SuperList:
-    def __init__(self, number):
-        self.numbers = [number]
+class SuperList(list):
+    # NOTE: We don't need this now that we're inheriting from the "list" (the "list" is the superclass now)
+    # def __init__(self, number):
+    #     self.numbers = [number]
 
     def __len__(self):
         return 1000
 
-    # def addNumber(self, number):
+    # NOTE: We don't need this now that we're inheriting from the "list" (the "list" is the superclass now)
+    # def append(self, number):
     #     self.numbers.append(number)
 
-    def append(self, number):
-        self.numbers.append(number)
-
-    def getNumberByPosition(self, position):
-        return self.numbers[position]
+    # NOTE: We don't need this now that we're inheriting from the "list" (the "list" is the superclass now)
+    # def getNumberByPosition(self, position):
+    #     return self.numbers[position]
 
 
-sl = SuperList(4)
+sl = SuperList()
+print(len(sl))
+
 # sl.addNumber(7)
 sl.append(7)
+sl.append(4)
 # print(len([3, 2, 3]))
-print(len(sl))
-print(f"Number on 2nd position is: {sl.getNumberByPosition(1)}")
+# print(f"Number on 2nd position is: {sl.getNumberByPosition(1)}")
+print(f"Number on 2nd position is: {sl[1]}")
